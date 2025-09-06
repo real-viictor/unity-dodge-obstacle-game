0e933e5ddecf17aa3a2955c08e8a793b8b9ff384
Victor Real <victor.realchisto30@hotmail.com>
2025-09-06 16:32:37 -0300

    created player movement system

--- Dodge Obstacle/Assets/Scripts/PlayerController.cs
+++ Dodge Obstacle/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,15 @@
+using System.Xml.Schema;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    private float xInput, zInput;
+    
+    [SerializeField] protected float playerSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        xInput = Input.GetAxis("Horizontal") * playerSpeed;
+        zInput = Input.GetAxis("Vertical") * playerSpeed;
+        transform.Translate(xInput, 0, zInput);
     }
 }
